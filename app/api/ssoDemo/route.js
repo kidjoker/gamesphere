29008d04c030b97bef7fa9f304508b565127f0a0
kidjoker <superherojoke@gmail.com>
2024-06-13 14:41:42 -0500

    final phase two

--- app/api/ssoDemo/route.js
+++ app/api/ssoDemo/route.js
@@ -2,8 +2,11 @@ import dbConnect from '@/lib/mongoose';
 import User from '@/models/User';
 import authMiddleware from '@/lib/auth';
 import { NextResponse } from 'next/server';
+import { applyCors } from '@/lib/cors';
 
 const handler = async (req, res) => {
+  if (applyCors(req, res)) return;
+
   await dbConnect();
 
   const user = await User.findById(req.user.id);
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            //CSP
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' https://trusted.cdn.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:",
+          },
+          //X-Frame
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          //HSTS
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          //Content-type
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
